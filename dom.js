11db23db8d678e1d8addb3baa1a04c34138b6110
Masha <m.aldosary@safcsp.org.sa>
2022-09-01 14:46:15 +0300

    change in dom file

--- dom.js
+++ dom.js
@@ -1,6 +1,7 @@
-// let test1 = document.getElementById("root").innerHTML="Hello World";
-//  test1 = document.getElementById("root").style.color="red";
-// let body = document.body.style.backgroundColor="green"
+let test1 = document.getElementById("root")
+test1.innerHTML="Hello World";
+ test1 .style.color="red";
+let body = document.body.style.backgroundColor="green"
 
 function test(){
     // test1 = document.getElementById("root").innerHTML="Buton change";
